891fed6d2939b3052f2c4d77f4eef0800ac6f253
Sena <senaa.akpinarr@gmail.com>
2020-11-11 16:30:52 +0300

    api işlemleri halledildi

--- weather-forecast.js
+++ weather-forecast.js
@@ -1,43 +1,52 @@
 let cities = document.getElementById("cities");
-console.log("cities",cities);
+let image = document.getElementById("img");
+let newCity;
 
-$(document).ready(function(){
-    $('#addBtn').click(function(e){
-        const newCity = cities.value.trim();
-        var card = $("<div class='card'>"+newCity+"</div>");
-        // card.html(newCity + 'Derece');
-        console.log("new city",newCity);
-        card.appendTo('.card');
+$(document).ready(function (newCity) {
+    $('#addBtn').click(function (e) {
+        newCity = cities.value.trim();
+
+        $.ajax({
+            url: 'http://api.weatherstack.com/current',
+            data: {
+                access_key: '885c6c35d49e96bcdc2a7177dd39fac7',
+                query: newCity
+            },
+            dataType: 'json',
+            success: function (apiResponse) {
+                // var items = [];
+                console.log(apiResponse);
+                card = $("<form class='form '><div class='card col-lg-12'><button class='btn btn-danger px-1' id='btnRemove'>X</button>" + `${apiResponse.location.name} ${apiResponse.current.temperature}` + " °C" + "</div></from>");
+                card.appendTo('.form');
+                var silinecekCard = `${apiResponse.location.name}`
+                if (silinecekCard == newCity) {
+                    $(document).ready(function (e) {
+                        $('.btn-danger').click(function (e) {
+                            silinecekCard.Remove();
+                            console.log("silme işlemine gidiliyor", silinecekCard);
+
+                        });
+                    });
+
+                    silinecekCard.Remove();
+                }
+                console.log(silinecekCard);
+
+
+
+            }
+        });
     });
 });
 
-// $.getJSON("https://api.collectapi.com/weather/getWeather?data.lang=tr&data.city=ankara/oEyw9kmfsMOpJ8VZfdXk1:2Nqz8Zi0qhk5NJv0BKOQg4", function(data){
-//     var items =[];
-//     $.each(data,function(key,val){
-//         items.push("<div class='card'>"+newCity+"</div>");
-//     });
-//     $("",{
-//         "class":"card",
-//         html: items.join("")
-//     }).appendTo("body");
-//     console.log(data);
-// });
-$.ajax({
-    beforeSend: function(request) {
-        request.setRequestHeader("content-type", "application/json");
-        request.setRequestHeader("authorization", " oEyw9kmfsMOpJ8VZfdXk1:2Nqz8Zi0qhk5NJv0BKOQg4");
-    },
-    dataType: "json",
-    url: "https://api.collectapi.com/weather/getWeather?data.lang=tr&data.city=ankara",
-    success: function(data) {
-         var items =[];
-        $.each(data,function(key,val){
-             items.push("<div class='card'>"+newCity+"</div>");
-            });
-         $("",{
-           "class":"card",
-             html: items.join("")
-           }).appendTo("body");
-          console.log(data);    }
-    });
+// function cardRemove(silinecekCard){
+//     // silme işlemleri
+//     silinecekCard.Remove();
+//     console.location("card silindi",silinecekCard);
+// }
 
+
+// $("", {
+//     class: "card",
+//     html: items.join("")
+//  }).appendTo("body");
